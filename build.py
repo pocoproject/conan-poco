0b49022297a4848d6ec11d7222d41d7266e6def2
Viknash <viknash@hobbiz.com>
2018-05-22 23:50:05 +0800

    [Test][Travis] Test

--- build.py
+++ build.py
@@ -3,13 +3,5 @@ from conan.packager import ConanMultiPackager
 
 if __name__ == "__main__":
     builder = ConanMultiPackager()
-    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
-                options={"*:shared": True}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
-                options={"*:shared": True}, env_vars={}, build_requires={})    
-    builder.add(settings={"arch": "x86_64", "build_type": "Release"},
-                options={}, env_vars={}, build_requires={})
-    builder.add(settings={"arch": "x86_64", "build_type": "Debug"},
-                options={}, env_vars={}, build_requires={})    
-    #builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
+    builder.add_common_builds(shared_option_name="Poco:shared", pure_c=False)
     builder.run()
